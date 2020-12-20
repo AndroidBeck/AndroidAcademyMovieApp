ddc661720b34e9d36cad9041b4f286b61cba1587
Aleksandr Evdokimov <alexandr8@gmail.com>
2020-12-20 13:21:06 +0300

    Add ActorsAdapter.kt

---
+++ app/src/main/java/ru/aevd/androidacademymovieapp/ActorsAdapter.kt
@@ -0,0 +1,27 @@
+package ru.aevd.androidacademymovieapp
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+
+class ActorsAdapter: RecyclerView.Adapter<ActorViewHolder>() {
+    private var actors: List<Actor> = listOf()
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ActorViewHolder {
+        val view: View = LayoutInflater.from(parent.context)
+                .inflate(R.layout.view_holder_actor, parent, false)
+        return ActorViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: ActorViewHolder, position: Int) {
+        holder.onBind(actors[position])
+    }
+
+    override fun getItemCount(): Int = actors.size
+
+    fun bindActors(newActors: List<Actor>) {
+        actors = newActors
+    }
+
+}
