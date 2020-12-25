15805fb80e0388fa5920f472798ee9a5863c1280
Aleksandr Evdokimov <alexandr8@gmail.com>
2020-12-25 14:03:31 +0300

    Ad assets folder with json's Add data folder with new Actor.kt, Movie.kt; also with Genre.kt and JsonLoad.kt

---
+++ app/src/main/java/ru/aevd/androidacademymovieapp/data/Actor.kt
@@ -0,0 +1,7 @@
+package ru.aevd.androidacademymovieapp.data
+
+data class Actor(
+    val id: Int,
+    val name: String,
+    val picture: String
+)
---
+++ app/src/main/java/ru/aevd/androidacademymovieapp/data/Genre.kt
@@ -0,0 +1,3 @@
+package ru.aevd.androidacademymovieapp.data
+
+data class Genre(val id: Int, val name: String)
