7f105d0c75e0ffdc932aff8d8705deb35cd96554
Aleksandr Evdokimov <alexandr8@gmail.com>
2021-03-24 16:02:06 +0300

    Remove unused stuff

--- app/src/main/java/ru/aevd/androidacademymovieapp/ui/viewmodels/MoviesDetailsViewModelFactory.kt
+++ app/src/main/java/ru/aevd/androidacademymovieapp/ui/viewmodels/MoviesDetailsViewModelFactory.kt
@@ -5,8 +5,8 @@ import androidx.lifecycle.ViewModelProvider
 import ru.aevd.androidacademymovieapp.domain.entities.Movie
 
 class MoviesDetailsViewModelFactory(
-        private val movie: Movie
-        ): ViewModelProvider.Factory {
+    private val movie: Movie
+): ViewModelProvider.Factory {
     @Suppress("UNCHECKED_CAST")
     override fun <T : ViewModel?> create(modelClass: Class<T>): T = when(modelClass) {
         MoviesDetailsViewModel::class.java -> MoviesDetailsViewModel(movie)
